646e96dadaac474b896e355b90d17170139fe88f
Claude Lee <liccbetter@gmail.com>
2015-03-30 16:50:00 +0800

    add clone example

---
+++ src/licc/utils/example/objclone/Rectangle.java
@@ -0,0 +1,57 @@
+package licc.utils.example.objclone;
+
+/**
+ * Created by user on 2015/3/30.
+ */
+public class Rectangle implements Cloneable {
+    int width;
+    int height;
+
+    public Rectangle(int w, int h) {
+        width = w;
+        height = h;
+    }
+
+    public void setWidth(int w) {
+        width = w;
+    }
+
+    public void setHeight(int h) {
+        height = h;
+    }
+
+    public int area() {
+        return width * height;
+    }
+
+    @Override
+    protected Rectangle clone() throws CloneNotSupportedException {
+        return (Rectangle)super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle Format [width=" + this.width + ",height=" + this.height + ",area=" + this.area() + "]";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (obj.getClass() != this.getClass()) return false;
+
+        Rectangle r = (Rectangle) obj;
+        if (this.width != r.width) return false;
+        if (this.height != r.height) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 37;
+        hash = hash * 47 + width;
+        hash = hash * 47 + height;
+        return hash;
+    }
+}
---
+++ src/licc/utils/example/objclone/RectangleTest.java
@@ -0,0 +1,33 @@
+package licc.utils.example.objclone;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RectangleTest {
+
+    @Test
+    public void testClone() throws Exception {
+        Rectangle rec = new Rectangle(30, 40);
+        Rectangle copy = null;
+
+        try {
+            copy = rec.clone();
+        } catch (CloneNotSupportedException e) {
+            System.out.println("[Clone not support:" + Rectangle.class.getName() + "]," + e.getMessage());
+        }
+
+        assert (rec != copy);
+        assert (rec.getClass() == copy.getClass());
+        assert (rec.equals(copy));
+        assert (rec.toString().equals(copy.toString()));
+
+        System.out.println(copy.toString());
+        copy.setWidth(10);
+        copy.setHeight(20);
+        System.out.println(copy.toString());
+
+        assert (!rec.equals(copy));
+
+    }
+}
