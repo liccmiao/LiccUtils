0b3e3ee500273d82ac7045f0bcad6cc88647b28a
liccbetter <liccbetter@gmail.com>
2015-07-14 22:43:01 +0800

    hackerrank optimized median of input stream (use two heaps, no sorting)

--- src/licc/utils/questions/hackerrank/MedianOfInputStream.java
+++ src/licc/utils/questions/hackerrank/MedianOfInputStream.java
@@ -1,5 +1,8 @@
 package licc.utils.questions.hackerrank;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 /**
@@ -30,5 +33,41 @@ public class MedianOfInputStream {
         }
     }
 
+    /**
+     * use two heaps
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+
+        Comparator<Integer> reverseCpt = Collections.reverseOrder();
+        PriorityQueue<Integer> h1Max = new PriorityQueue<>((N + 1) / 2, reverseCpt);
+        PriorityQueue<Integer> h2Min = new PriorityQueue<>(N - (N + 1) / 2);
+
+        for (int i = 0; i < N; ++i) {
+            int k = in.nextInt();
+            if ((i & 0x01) == 0) {
+                if (h2Min.isEmpty() || k <= h2Min.peek())
+                    h1Max.offer(k);
+                else {
+                    h1Max.offer(h2Min.poll());
+                    h2Min.offer(k);
+                }
+                // get h1.peek as the i/2+1 th element
+                System.out.println(String.format("%.1f", h1Max.peek() * 1.0f));
+            } else {
+                if (h1Max.isEmpty() || k >= h1Max.peek())
+                    h2Min.offer(k);
+                else {
+                    h2Min.offer(h1Max.poll());
+                    h1Max.offer(k);
+                }
+                //get h1.peek as the i/2+1 th element
+                //get h2.peek as the i/2+2 th element
+                System.out.println(String.format("%.1f", (h1Max.peek() + h2Min.peek()) * 1.0f / 2));
+            }
+        }
+
+    }
 
 }
